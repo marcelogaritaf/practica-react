356812697da87f69060374780e496095cc55020d
Marcelo Garita <mgaritaf@outlook.com>
2023-06-06 19:26:11 -0600

    style and menu

---
+++ src/components/Menu.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './styles.css'
+const Menu = () => {
+    const menuFunction = ()=>{
+        document.body.classList.toggle('open')
+    }
+  return (
+    <div>
+        <div className="background"></div>
+        <button className='burger' onClick={menuFunction}></button>
+        <div className="menu">
+            <nav>
+                <Link className='link' to="/">Carusel-1</Link>
+                <Link className='link'to="/carusel-2">Carusel-2</Link>
+                <Link className='link'to="/carusel-3">Carusel-3</Link>
+                <Link className='link'to="/carusel-4">Carusel-4</Link>
+                <Link className='link'to="/carusel-5">Carusel-5</Link>
+            </nav>
+        </div>
+    </div>
+  )
+}
+
+export default Menu
