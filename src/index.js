73f5aa3e05c73518fa71d83c3668f7298188c0f6
Marcelo Garita <mgaritaf@outlook.com>
2023-06-07 16:07:13 -0600

    changes and adding carusel-2

--- src/index.js
+++ src/index.js
@@ -24,3 +24,6 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+/**
+ *
+ */
--- src/routes/carusel-2/Carusel2.jsx
+++ src/routes/carusel-2/Carusel2.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import imagen1 from './assets/imagen1.jpg'
+import imagen2 from './assets/imagen2.jpg'
+import imagen3 from './assets/imagen3.jpg'
+import imagen4 from './assets/imagen4.jpg'
+import imagen5 from './assets/imagen5.jpg'
+import imagen6 from './assets/imagen6.jpg'
+import imagen7 from './assets/imagen7.jpg'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+
+import './stylesCarusel2.css'
+import 'swiper/css'
 
 const Carusel2 = () => {
+  const imagenes = [imagen1,imagen2,imagen3,imagen4,imagen5,imagen6,imagen7]
   return (
-    <div>Carusel2</div>
+    <div>
+      <Swiper 
+      spaceBetween={10} 
+      slidesPerView={3} 
+      loop 
+      >
+        {
+          imagenes.map(i=>(
+            <SwiperSlide key={i}>
+              <img src={i} alt='landspaces images'/>
+            </SwiperSlide>
+          ))
+        }
+      </Swiper>
+    </div>
   )
 }
 
